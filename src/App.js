c129458b10bfe6be275807c3fceb45ab6c89a2d5
princeantwi2k <antwi2k@gmail.com>
2022-05-19 22:49:18 +0800

    added App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Solution3 from './Component/Solution3';
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+   <Solution3 />
+    </div>
+  );
+}
+
+export default App;
